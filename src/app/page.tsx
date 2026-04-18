513aef710ea3b39be3491b7bfd0d17deaff55f4a
Chakib Benchinoun <74986065+ChakibBenchinoun@users.noreply.github.com>
2026-04-18 17:16:42 +0100

    Refactor import order and format code in page.tsx for consistency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,26 @@
-import { HeroSection } from "@/components/hero-section";
 import { AboutSection } from "@/components/about-section";
-import { SkillsSection } from "@/components/skills-section";
-import { ProjectsSection } from "@/components/projects-section";
 import { ContactSection } from "@/components/contact-section";
+import { HeroSection } from "@/components/hero-section";
+import { ProjectsSection } from "@/components/projects-section";
 import { ScrollArrow } from "@/components/scroll-arrow";
+import { SkillsSection } from "@/components/skills-section";
 
 export default function Home() {
-  return (
-    <div className="overflow-hidden">
-      {/* Hero Section */}
-      <HeroSection className="container mx-auto px-8 py-20 sm:py-[245px] lg:py-60 xl:py-[265px] mt-28" />
-      <div className="bg-gray-900 relative mb-20">
-        <ScrollArrow />
-        {/* About */}
-        <AboutSection className="container mx-auto px-8 py-32 sm:py-40 md:py-44 lg:py-48 xl:py-52 scroll-mt-20" />
-        {/* Skills */}
-        <SkillsSection className="container mx-auto px-8 py-32 sm:py-40 md:py-44 lg:py-48 xl:py-52 -scroll-mt-5" />
-        {/* Project Section */}
-        <ProjectsSection className="container mx-auto px-8 py-32 sm:py-40 md:py-44 lg:py-48 xl:py-52 -scroll-mt-10" />
-        {/* Let's Work Together */}
-        <ContactSection className="container mx-auto px-8 pb-32 sm:py-40 md:py-44 lg:py-48 xl:py-52 p-" />
-      </div>
-    </div>
-  );
+	return (
+		<div className="overflow-hidden">
+			{/* Hero Section */}
+			<HeroSection className="container mx-auto px-8 py-20 sm:py-[245px] lg:py-60 xl:py-[265px] mt-28" />
+			<div className="bg-gray-900 relative mb-20">
+				<ScrollArrow />
+				{/* About */}
+				<AboutSection className="container mx-auto px-8 py-32 sm:py-40 md:py-44 lg:py-48 xl:py-52 scroll-mt-20" />
+				{/* Skills */}
+				<SkillsSection className="container mx-auto px-8 py-32 sm:py-40 md:py-44 lg:py-48 xl:py-52 -scroll-mt-5" />
+				{/* Project Section */}
+				<ProjectsSection className="container mx-auto px-8 py-32 sm:py-40 md:py-44 lg:py-48 xl:py-52 -scroll-mt-10" />
+				{/* Let's Work Together */}
+				<ContactSection className="container mx-auto px-8 pb-32 sm:py-40 md:py-44 lg:py-48 xl:py-52 p-" />
+			</div>
+		</div>
+	);
 }
