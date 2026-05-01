9be7041b13328ecf3af1816d3592b32ceb144154
Chakib Benchinoun <74986065+ChakibBenchinoun@users.noreply.github.com>
2026-05-02 00:25:15 +0100

    refactor: reorder imports and clean up formatting in ContactSection component

--- src/components/contact-section.tsx
+++ src/components/contact-section.tsx
@@ -1,58 +1,56 @@
 "use client";
 
-import { socialNetwork } from "@/data";
 import clsx from "clsx";
 import { motion } from "framer-motion";
+import { socialNetwork } from "@/data";
 
 export function ContactSection({ className }: { className?: string }) {
-  return (
-    <div id="contact" className={className}>
-      <div>
-        <motion.div
-          initial={{ opacity: 0 }}
-          whileInView={{ opacity: 1 }}
-          transition={{ duration: 2, delay: 0.3 }}
-          className="w-fit mx-auto"
-        >
-          <h2 className="font-bold tracking-tight text-white text-3xl sm:text-4xl lg:text-5xl">
-            Contact Me
-          </h2>
-          <div className="bg-lime-500 w-1/2 h-1 mx-auto mt-2" />
-        </motion.div>
-      </div>
-      <div className="mt-24">
-        <motion.p
-          initial={{ opacity: 0 }}
-          whileInView={{ opacity: 1 }}
-          transition={{ duration: 2, delay: 0.4 }}
-          viewport={{ once: true }}
-          className="text-xl tracking-wider leading-loose sm:text-3xl text-gray-100 text-center max-w-2xl mx-auto"
-        >
-          Got a project in mind? Contact Me! Let&apos;s make something awesome
-          together.
-        </motion.p>
-        <div className="grid sm:grid-cols-2 justify-center sm:justify-items-center gap-10 text-base leading-7 mt-20 max-w-md sm:max-w-xl mx-auto">
-          {socialNetwork.map((item, i) =>
-            item.label === "Github" ? null : (
-              <motion.a
-                key={`${item.label}-${i}`}
-                initial={{ opacity: 0 }}
-                whileInView={{ opacity: 1 }}
-                viewport={{ once: true }}
-                transition={{ duration: 2, delay: 0.5 + i / 9 }}
-                href={item.path}
-              >
-                <h3 className="border-l-4 border-lime-500 pl-6 font-semibold text-gray-200 flex items-center">
-                  <item.icon /> <span className="ml-2">{item.label}</span>
-                </h3>
-                <address className="border-l-4 border-gray-400 pl-6 pt-2 not-italic text-gray-500">
-                  <p>{item.name}</p>
-                </address>
-              </motion.a>
-            )
-          )}
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div id="contact" className={className}>
+			<div>
+				<motion.div
+					initial={{ opacity: 0 }}
+					whileInView={{ opacity: 1 }}
+					transition={{ duration: 2, delay: 0.3 }}
+					className="w-fit mx-auto"
+				>
+					<h2 className="font-bold tracking-tight text-white text-3xl sm:text-4xl lg:text-5xl">
+						Contact Me
+					</h2>
+					<div className="bg-lime-500 w-1/2 h-1 mx-auto mt-2" />
+				</motion.div>
+			</div>
+			<div className="mt-24">
+				<motion.p
+					initial={{ opacity: 0 }}
+					whileInView={{ opacity: 1 }}
+					transition={{ duration: 2, delay: 0.4 }}
+					viewport={{ once: true }}
+					className="text-xl tracking-wider leading-loose sm:text-3xl text-gray-100 text-center max-w-2xl mx-auto"
+				>
+					Got a project in mind? Contact Me! Let&apos;s make something awesome
+					together.
+				</motion.p>
+				<div className="grid sm:grid-cols-2 justify-center sm:justify-items-center gap-10 text-base leading-7 mt-20 max-w-md sm:max-w-xl mx-auto">
+					{socialNetwork.map((item, i) => (
+						<motion.a
+							key={`${item.label}-${i}`}
+							initial={{ opacity: 0 }}
+							whileInView={{ opacity: 1 }}
+							viewport={{ once: true }}
+							transition={{ duration: 2, delay: 0.5 + i / 9 }}
+							href={item.path}
+						>
+							<h3 className="border-l-4 border-lime-500 pl-6 font-semibold text-gray-200 flex items-center">
+								<item.icon /> <span className="ml-2">{item.label}</span>
+							</h3>
+							<address className="border-l-4 border-gray-400 pl-6 pt-2 not-italic text-gray-500">
+								<p>{item.name}</p>
+							</address>
+						</motion.a>
+					))}
+				</div>
+			</div>
+		</div>
+	);
 }
